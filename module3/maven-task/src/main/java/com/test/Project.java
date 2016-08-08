6a6fb05610814dd9b85f785a14ee3b1a898ea63d
Anton Tkachenka <anton_tkachenka@epbyminw2468t1.(none)>
2016-08-08 15:29:04 +0300

    super-commit1

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -11,9 +11,6 @@ class Project {
   
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
-    System.out.println("Hellow Sonar");
-    System.out.println("Hellow Anton");
-    System.out.println("Hellow Yahor");
   }
 
 }
